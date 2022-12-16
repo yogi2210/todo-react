0e798228cc48da206511640d4bdebb466ff44d07
Yogesh <yogi.2210@gmail.com>
2022-12-16 23:41:54 +0530

    make a task component and add some basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState} from 'react'
+import {Task} from './Task'
 
 function App() {
   const [todoList, setTodoList] = useState([])
@@ -23,18 +24,15 @@ function App() {
 
   return (
     <div className="App">
-     <div className="addTask">
-      <input type="text" onChange={handleChange}  />
-      <button onClick={addTask} >ADD TASK</button>
-    
-     </div>
+      <div className='header' >
+        <div className="addTask">
+          <input type="text" onChange={handleChange}  />
+          <button onClick={addTask} >ADD TASK</button>
+         </div>
+      </div>
      <div className="list">
       {todoList.map((task)=>{
-        return ( <div>
-            <h1>{task.taskName}</h1>
-            <button onClick={() => deleteTask(task.id)} > X </button>
-
-          </div>)
+        return <Task taskName = {task.taskName} id = {task.id} deleteTask = {deleteTask} /> 
       })}
      </div>
     </div>
---
+++ src/Task.js
@@ -0,0 +1,8 @@
+export const Task = (props)=>{
+    return(
+        <div className='task-div' >
+            <h1>{props.taskName}</h1>
+            <button onClick={() => props.deleteTask(props.id)} > X </button>
+        </div>
+    )
+}
